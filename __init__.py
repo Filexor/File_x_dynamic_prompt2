399d0caf33693f9b5144630f753afb5a2c761780
File_xor <filexor@gmail.com>
2025-07-25 21:31:29 +0900

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from custom_nodes.File_x_dynamic_prompt2.File_x_dynamic_prompt2 import File_x_DynamicPrompt2
+
+__version__ = "0.0.0"
+
+NODE_CLASS_MAPPINGS = {
+    "File_x_DynamicPrompt2": File_x_DynamicPrompt2,
+}
